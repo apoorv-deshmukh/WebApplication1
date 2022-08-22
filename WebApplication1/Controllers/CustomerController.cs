57f7533cbaddce56e3cf074f14ff0304daa7d28c
Apoorv Deshmukh <apoorv0112@gmail.com>
2022-08-22 10:53:19 +0530

    added controller

---
+++ WebApplication1/Controllers/CustomerController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApplication1.Models;
+using WebApplication1.Services;
+
+namespace WebApplication1.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CustomerController : ControllerBase
+    {
+        private readonly IRepo<int, Customer> _repo;
+
+        public CustomerController(IRepo<int, Customer> repo)
+        {
+            _repo = repo;
+        }
+
+        public async Task<ActionResult<List<Customer>>> Get()
+        {
+            return Ok((await _repo.GetAll()).ToList());
+        }
+    }
+}
